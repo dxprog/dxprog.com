0ce69b8f4b30ece2f09743338e60128cbe5ebb29
Matt Hackmann <matt@dxprog.com>
2018-02-17 23:04:59 -0800

    updated to latest staticr

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
-import { PageGenerator } from 'staticr-site';
+import { SiteGenerator } from 'staticr-site';
 import * as path from 'path';
 
 import { DxApiReader } from './src/readers/dxapi-reader';
 
-const pg = new PageGenerator({
+const pg = new SiteGenerator({
   outputDir: path.join(process.cwd(), 'docs/')
 });
 
-//pg.addReader(DxApiReader);
+// pg.addReader(DxApiReader);
+pg.writer.addStaticContent('static', path.resolve(process.cwd(), 'static'));
 
 pg.build().then((result: any) => {
-
+  console.log('done');
 });
