e2a1c41eeb957e292fb3a52bf8cff446a3ae972b
Matt Hackmann <matt@dxprog.com>
2018-02-25 23:24:41 -0800

    [break] tag is honored

--- src/renderers/post-page-renderer.tsx
+++ src/renderers/post-page-renderer.tsx
@@ -22,13 +22,17 @@ const { Post } = Components;
 export const PostPageRenderer: IRenderer = {
   renderPosts(posts: Array<IPost>, siteGenerator: SiteGenerator): Promise<Array<IRenderedPage>> {
     return Promise.resolve(posts.map((post: IPost) => {
+      // Create a copy so we can remove any break tag in the post
+      const postCopy = Object.create(post);
+      postCopy.html = postCopy.html.replace('[break]', '');
+
       return {
         title: post.attributes.title,
         path: `entry/${post.attributes.slug}`,
         pageComponent: (
           <section className="post-page">
             <IntroBar siteGenerator={siteGenerator} orientation={IntroBarOrientation.Horizontal} />
-            <Post post={post} siteGenerator={siteGenerator} />
+            <Post post={postCopy} siteGenerator={siteGenerator} />
             <Footer />
           </section>
         )
--- src/renderers/rollup-page-renderer.tsx
+++ src/renderers/rollup-page-renderer.tsx
@@ -11,6 +11,7 @@ import {
 import { IntroBarOrientation } from '../components/intro-bar';
 import { Page } from '../components/page';
 
+const BREAK_TAG = '[break]';
 const POSTS_PER_PAGE = 5;
 const { PostsRollupRenderer } = Renderers;
 
@@ -23,12 +24,24 @@ export const RollupPageRenderer: IRenderer = {
 
     const pages: Array<IRenderedPage> = [];
     rollupRender.iteratePostPages(posts, (postsPage: IPostsRollupPage) => {
+      const path = `archives/${postsPage.pageNum}`;
+
+      // Create a deep copy of the posts array so we can honor the [break]
+      // tag without mutating data down the line
+      const postsCopy: Array<IPost> = postsPage.posts.map(post => Object.create(post));
+      postsCopy.forEach(post => {
+        // TODO - Think of a cleaner way to do this...
+        if (post.html.indexOf(BREAK_TAG) > -1) {
+          post.html = `${post.html.split(BREAK_TAG)[0]}\n\n<p><a href="${siteGenerator.generateUrl(`entry/${post.attributes.slug}`)}">Continue Reading</a></p>`;
+        }
+      });
+
       pages.push({
         title: `Archives - Page ${postsPage.pageNum}`,
-        path: `archives/${postsPage.pageNum}`,
+        path,
         pageComponent: (
           <Page
-            posts={postsPage.posts}
+            posts={postsCopy}
             siteGenerator={siteGenerator}
             previousPage={postsPage.previousPage}
             nextPage={postsPage.nextPage}
