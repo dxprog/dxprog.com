c311fcd2f073da2db43278b382d5b7fb9fa23801
Matt Hackmann <matt.hackmann@gmail.com>
2024-12-28 18:44:56 -0800

    convert footer to functional component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,16 +1,9 @@
 import * as React from 'react';
-import { ReactNode } from 'react';
-import { SiteGenerator } from 'staticr-site';
 
-export class Footer extends React.Component<undefined, undefined> {
-
-  render(): ReactNode {
-    return (
-      <footer className="footer">
-        <p className="footer__copyright">Copyright &copy; {(new Date()).getFullYear()} Matt Hackmann</p>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-280226-1"></script>
-        <script src="//dxprog.com/static/js/dxprog.js"></script>
-      </footer>
-    );
-  }
-}
+export const Footer = (): React.ReactNode => (
+  <footer className="footer">
+    <p className="footer__copyright">Copyright &copy; {(new Date()).getFullYear()} Matt Hackmann</p>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-280226-1"></script>
+    <script src="//dxprog.com/static/js/dxprog.js"></script>
+  </footer>
+);
