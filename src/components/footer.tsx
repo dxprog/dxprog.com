5541a6f9cb898838f3cb3a8fa345f3db915d3206
Matt Hackmann <matt.hackmann@gmail.com>
2025-01-16 22:14:06 -0800

    a bunch of design tweaks and updates

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 
+import { SocialLinks } from './SocialLinks';
+
 export const Footer = (): React.ReactNode => (
   <footer className="footer">
+    <SocialLinks />
     <p className="footer__copyright">Copyright &copy; {(new Date()).getFullYear()} Matt Hackmann</p>
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-280226-1"></script>
     <script src="//dxprog.com/static/js/dxprog.js"></script>
