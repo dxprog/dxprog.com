ceac31f920b1a5eac9f8525421352c3adebd3299
Matt Hackmann <matt.hackmann@gmail.com>
2025-01-16 22:15:06 -0800

    a bunch of design tweaks and updates (#34)

---
+++ src/components/SocialLinks.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+
+export const SocialLinks = () => (
+  <ul className="social__links">
+    <li className="social__link"><a href="https://github.com/dxprog" target="_blank">GitHub</a></li>
+    <li className="social__link"><a href="https://www.youtube.com/@dxprog" target="_blank">YouTube</a></li>
+    <li className="social__link"><a href="https://www.instagram.com/hakkslab" target="_blank">Instagram</a></li>
+    <li className="social__link"><a href="https://www.linkedin.com/in/mhackmann" target="_blank">LinkedIn</a></li>
+  </ul>
+);
--- src/components/intro-bar.tsx
+++ src/components/intro-bar.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { SiteGenerator } from 'staticr-site';
 
-import { SocialLink } from './SocialLink';
+import { SocialLinks } from './SocialLinks';
 
 export enum IntroBarVariant {
   Small = 'small',
@@ -17,26 +17,16 @@ export const IntroBar = (
   { siteGenerator, variant = IntroBarVariant.Full }: IntroBarProps
 ): React.ReactNode => (
   <section className={`intro-bar intro-bar--${variant}`}>
-    <img src={siteGenerator.generateUrl('static/images/me.jpg')} alt="Matt Hackmann" className="intro-bar__photo" />
+    <img src={siteGenerator.generateUrl('static/images/me-2025.jpg')} alt="Matt Hackmann" className="intro-bar__photo" />
     <h1 className="intro-bar__header">
       <a href={siteGenerator.generateUrl('')} title="Back to home" className="intro-bar__home-link">
         <span className="intro-bar__name intro-bar__name--first">Matt</span>
         <span className="intro-bar__name intro-bar__name--last">Hackmann</span>
       </a>
     </h1>
-    <h2 className="intro-bar__subhead">The thoughts and goings-on of some programmer dude.</h2>
+    <h2 className="intro-bar__subhead">The thoughts and goings-on of some programmer dad.</h2>
     <nav className="intro-bar__social-nav">
-      <ul className="social-links">
-        <SocialLink classModifier="rss" href="http://feeds.feedburner.com/dxprog">
-          RSS Feed
-        </SocialLink>
-        <SocialLink classModifier="github" href="https://github.com/dxprog">
-          GitHub
-        </SocialLink>
-        <SocialLink classModifier="linkedin" href="https://www.linkedin.com/in/mhackmann">
-          LinkedIn Profile
-        </SocialLink>
-      </ul>
+      <SocialLinks />
     </nav>
   </section>
 );
